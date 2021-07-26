35391dd601eeb0ad2adab2c0701e9546c94a8c92
Andy <webmaster@RealityRipple.com>
2021-07-26 13:43:18 -0700

    Use URLBar Icon
    No more immediate redirect to translations.
    Translate icon shows up if different language is detected.
    Clicking translate icon toggles between translated and untranslated.
    Middle-clicking translate icon opens translation in a new tab.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,7 +1,65 @@
 var babbleon_overlay = {
  LoadListener: function()
  {
+  babbleon_overlay.makeButton(window);
   gBrowser.addTabsProgressListener(babbleon_overlay.ProgressListener, Components.interfaces.nsIWebProgress.NOTIFY_PROGRESS);
+  window.getBrowser().addProgressListener(babbleon_overlay.LocationListener);
+ },
+ LocationListener:
+ {
+  onLocationChange: function(aProgress, aRequest, aLocation, aFlags)
+  {
+   var lang = null;
+   try
+   {
+    lang = aProgress.DOMWindow.document.documentElement.attributes.lang.value;
+   }
+   catch (e) {lang = null;}
+   if (lang === null)
+   {
+    babbleon_overlay.hideButton(window);
+    return;
+   }
+   if (aLocation !== null)
+   {
+    if (aLocation.asciiHost.slice(-15) === '.translate.goog')
+    {
+     babbleon_overlay.showButton(window, true);
+     return;
+    }
+   }
+   if (aProgress.DOMWindow !== null)
+   {
+    if (aProgress.DOMWindow.location.hostname.slice(-15) === '.translate.goog')
+    {
+     babbleon_overlay.showButton(window, true);
+     return;
+    }
+   }
+   var found = false;
+   let findLang = lang.toLowerCase();
+   if (findLang.indexOf('-') != -1)
+    findLang = findLang.slice(0, findLang.indexOf('-'));
+   if (findLang.indexOf('_') != -1)
+    findLang = findLang.slice(0, findLang.indexOf('_'));
+   for (myLang of navigator.languages)
+   {
+    let matchLang = myLang.toLowerCase();
+    if (matchLang.indexOf('-') != -1)
+     matchLang = matchLang.slice(0, matchLang.indexOf('-'));
+    if (matchLang.indexOf('_') != -1)
+     matchLang = matchLang.slice(0, matchLang.indexOf('_'));
+    if (matchLang == findLang)
+    {
+     found = true;
+     break;
+    }
+   }
+   if (found)
+    babbleon_overlay.hideButton(window);
+   else
+    babbleon_overlay.showButton(window, false);
+  }
  },
  ProgressListener:
  {
@@ -13,10 +71,34 @@ var babbleon_overlay = {
     return this;
    throw Components.results.NS_NOINTERFACE;
   },
-  onLocationChange: async function() {},
-  onStateChange: function() {},
-  onProgressChange: function() {},
-  onStatusChange: function(aBrowser, aProgress, aRequest, aStatus, aMessage) {
+  onStatusChange: function(aBrowser, aProgress, aRequest, aStatus, aMessage)
+  {
+   let matchedTab = false;
+   for (let i = 0; i < gBrowser.tabs.length; i++)
+   {
+    if (!gBrowser.tabs[i].hasAttribute('selected') || gBrowser.tabs[i].getAttribute('selected') !== 'true')
+     continue;
+    if (!aBrowser.hasOwnProperty('registeredOpenURI'))
+     continue;
+    if (aBrowser.registeredOpenURI.spec === gBrowser.tabs[i].linkedBrowser.registeredOpenURI.spec)
+    {
+     matchedTab = true;
+     break;
+    }
+   }
+   if (!matchedTab)
+    return;
+   var uri = null;
+   try
+   {
+    uri = aBrowser.contentWindow.document.documentURIObject;
+    if (uri.asciiHost.slice(-15) == '.translate.goog')
+    {
+     babbleon_overlay.showButton(window, true);
+     return;
+    }
+   }
+   catch (e) {}
    var lang = null;
    try
    {
@@ -45,15 +127,102 @@ var babbleon_overlay = {
     }
    }
    if (found)
+   {
+    babbleon_overlay.hideButton(window);
     return;
-   var uri = null;
+   }
+   babbleon_overlay.showButton(window, false);
+  },
+  onSecurityChange: function() {},
+  onLinkIconAvailable: function() {}
+ },
+ makeButton: function(wnd)
+ {
+  let urlBarIconsBox = wnd.document.getElementById('urlbar-icons');
+  if (!urlBarIconsBox)
+   return;
+  let spaceHeight = urlBarIconsBox.clientHeight;
+  let newIcon = wnd.document.createElement('image');
+  newIcon.setAttribute('id', 'babbleon-button');
+  newIcon.setAttribute('class', 'urlbar-icon');
+  newIcon.setAttribute('style', 'overflow: hidden; display: none;');
+  newIcon.setAttribute('tooltiptext', '');
+  newIcon.addEventListener('click', babbleon_overlay.toggleTranslation, false);
+  let starButton = urlBarIconsBox.querySelector('#star-button');
+  urlBarIconsBox.insertBefore(newIcon,starButton);
+ },
+ showButton: function(wnd, translated)
+ {
+  let translateButton = wnd.document.getElementById('babbleon-button');
+  if (!translateButton)
+   return;
+  let locale = Components.classes['@mozilla.org/intl/stringbundle;1'].getService(Components.interfaces.nsIStringBundleService).createBundle('chrome://babbleon/locale/babbleon.properties');
+  if (translated)
+  {
+   if (translateButton.classList.contains('untranslated'))
+    translateButton.classList.remove('untranslated');
+   if (!translateButton.classList.contains('translated'))
+    translateButton.classList.add('translated');
+   translateButton.setAttribute('tooltiptext', locale.GetStringFromName('button.undo.tooltip'));
+  }
+  else
+  {
+   if (translateButton.classList.contains('translated'))
+    translateButton.classList.remove('translated');
+   if (!translateButton.classList.contains('untranslated'))
+    translateButton.classList.add('untranslated');
+   translateButton.setAttribute('tooltiptext', locale.GetStringFromName('button.do.tooltip'));
+  }
+  translateButton.style.display = 'inline-block';
+ },
+ hideButton: function(wnd)
+ {
+  let translateButton = wnd.document.getElementById('babbleon-button');
+  if (!translateButton)
+   return;
+  translateButton.style.display = 'none';
+  if (translateButton.classList.contains('translated'))
+   translateButton.classList.remove('translated');
+  if (translateButton.classList.contains('untranslated'))
+   translateButton.classList.remove('untranslated');
+  translateButton.setAttribute('tooltiptext', '');
+ },
+ toggleTranslation: function(evt)
+ {
+  if (evt.button === 2)
+   return;
+  let toURL = null;
+  let selWnd = window.getBrowser().selectedBrowser;
+  if (selWnd.currentURI.asciiHost.slice(-15) == '.translate.goog')
+  {
+   let base = null;
+   for (let i = 0; i < selWnd.contentDocument.head.children.length; i++)
+   {
+    if (selWnd.contentDocument.head.children[i].tagName !== 'BASE')
+     continue;
+    base = selWnd.contentDocument.head.children[i].href;
+    break;
+   }
+   if (selWnd.currentURI.ref !== '')
+    base += '#' + selWnd.currentURI.ref;
+   toURL = base;
+  }
+  else
+  {
+   var lang = null;
    try
    {
-    uri = aBrowser.contentWindow.document.documentURIObject;
-    if (uri.asciiHost.slice(-15) == '.translate.goog')
-     return;
+    lang = selWnd.contentDocument.documentElement.attributes.lang.value;
    }
    catch (e) {lang = null;}
+   if (lang === null)
+    return;
+   let uri = null;
+   try
+   {
+    uri = selWnd.currentURI;
+   }
+   catch (e) {uri = null;}
    if (uri == null)
     return;
    var newURL = 'https://' + uri.asciiHost.replaceAll('.', '-') + '.translate.goog' + uri.filePath;
@@ -62,10 +231,22 @@ var babbleon_overlay = {
     newURL += '&' + uri.query;
    if (uri.ref != '')
     newURL += '#' + uri.ref;
-   aBrowser.contentWindow.location = newURL;
-  },
-  onSecurityChange: function() {},
-  onLinkIconAvailable: function() {}
- },
+   toURL = newURL;
+  }
+  if (evt.button === 1)
+  {
+   let mdtr = Components.classes['@mozilla.org/appshell/window-mediator;1'].getService(Components.interfaces.nsIWindowMediator);
+   let rwnd = mdtr.getMostRecentWindow('navigator:browser');
+   if (rwnd)
+   {
+    let nw = rwnd.gBrowser.addTab(toURL, null, null, null, null, null);
+    rwnd.gBrowser.selectedTab = nw;
+   }
+   else
+    window.open(toURL);
+  }
+  else
+   selWnd.contentWindow.location = toURL;
+ }
 };
 window.addEventListener('load', babbleon_overlay.LoadListener, false);
